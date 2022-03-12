27a71dc1828c74f29c3661c52accbcf57bcc5769
OwlFight <emilien.taisne@gmail.com>
2022-03-12 16:41:09 -0500

    breakBlock() returns the time to wait for the block to break

--- Assets/Scripts/Digging/BlockBreaking.cs
+++ Assets/Scripts/Digging/BlockBreaking.cs
@@ -6,30 +6,41 @@ using UnityEngine;
 public class BlockBreaking : MonoBehaviour
 {
     //This is should be called when the player wants this block
-    public void breakBlock(){
+    public float breakBlock(){
+        float breakTime = 0.1f;
         //Stuff that happens when breaking the block
         switch(this.tag){
             case "Health":
                 //Code executed when breaking a HEALTH block
+                breakTime = 0.5f;
                 break;
 
             case "FossiFuel":
                 //Code executed when breaking a FOSSIL FUEL block
+                breakTime = 0.5f;
                 break;
 
             case "Bomb":
                 //Code executed when breaking a BOMB block
+                breakTime = 1f;
                 break;
 
             case "Monster":
                 //Code executed when breaking a MONSTER block
+                breakTime = 2f;
                 break;
 
             case "Fossil":
                 //Code executed when breaking a FOSSIL block
+                breakTime = 1f;
                 break;
         }
         //kill the gameobject
+        StartCoroutine(KillBlock(breakTime));
+        return breakTime;
+    }
+    IEnumerator KillBlock(float waitTime){
+        yield return new WaitForSeconds(waitTime);
         Destroy(this);
     }
 }
