4e12dfa9c75a6b3314c364f530b1bc2b25062988
Nick Stewart <nicktstewart@gmail.com>
2022-03-11 20:12:56 -0500

    [Add] hp stuff halfway

---
+++ Assets/Scripts/UI/HP.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+
+    public static int playerHp = 0;
+
+    void Update()
+    {
+        // set hp size
+    }
+}
