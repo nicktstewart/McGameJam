bb57ea34af1d369d1226fc3f258c66323c92a531
Nick Stewart <nicktstewart@gmail.com>
2022-03-13 13:52:27 -0400

    [Add] GameOver2

---
+++ Assets/GoToBossFIght.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GoToBossFIght : MonoBehaviour
+{
+    public GameObject fadeBlack;
+    private bool hasClicked = false;
+    public void OnButtonClick()
+    {
+        if (!hasClicked)
+            StartCoroutine(FadeToBlack());
+        hasClicked = true;
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        fadeBlack.SetActive(true);
+        for (int i = 0; i < 30; i++)
+        {
+            fadeBlack.GetComponent<Image>().color = new Color(1, 1, 1, i / 30.0f);
+            yield return new WaitForSeconds(0.02f);
+        }
+        SceneManager.LoadScene("BossFight");
+    }
+}
