26b076f16c7fc88a650865c78f141fef571b1c3b
Nick Stewart <nicktstewart@gmail.com>
2022-03-13 13:47:46 -0400

    [Fix] DontCLick

--- Assets/Scripts/Menu/DontClickURL.cs
+++ Assets/Scripts/Menu/DontClickURL.cs
@@ -7,7 +7,11 @@ public class DontClickURL : MonoBehaviour
     public AudioSource a;
     public void OnButtonPress()
     {
+        float v = PlayerPrefs.GetFloat("BGMVolume", 0.1f);
+        PlayerPrefs.SetFloat("BGMVolume", 0);
+        BGMController.bgmvolume = 0;
         print("RICKROLL");
+        a.volume = v;
         a.Play();
     }
 }
